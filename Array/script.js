4c5fbedc70862fd22c232f32b5e2da82944d29bb
atholehansraj <hansrajathole1@gmail.com>
2024-12-11 23:28:38 +0530

    Array Question

---
+++ Array/script.js
@@ -0,0 +1,29 @@
+
+// let arr = [10, 20, 30, 40, 50]
+// let temp = arr[0]
+
+// for(let i = 0 ; i<arr.length-1 ; i++){
+    //     arr[i]=arr[i+1]
+    // }
+    // arr[arr.length-1] = temp
+    // console.log(arr);
+    
+    
+// let arr = [10, 20, 30, 40, 50]
+// let n =3
+// let  temp =  arr[arr.length-1]
+
+// for(let i = arr.length-1 ; i>0 ; i--){
+//     arr[i]=arr[i-1]
+// }
+// arr[0] = temp
+// console.log(arr);
+
+// for(let k =1 ; k<= n ; k++){
+//     for(let i = arr.length-1 ; i>0 ; i--){
+//         arr[i]=arr[i-1]
+//     }
+//     arr[0] = temp
+//     temp = arr[arr.length-1]
+// }
+// console.log(arr);
--- task-1/script.js
+++ task-1/script.js
@@ -127,10 +127,10 @@
 //     let hcf = 0
 //     let small = num1 < num2 ? num1 : num2
 
-//     for (var i = small; i > 0; i--){
+//     for (var i = 1; i <=small ; i++){
 //         if (num1 % i == 0 && num2 % i == 0){
 //             hcf = i
-//             break;
+//             // break;
 //         }
 //     }
     
@@ -346,32 +346,3 @@
 // }
 
 
-
-// let arr = [10, 20, 30, 40, 50]
-// let temp = arr[0]
-
-// for(let i = 0 ; i<arr.length-1 ; i++){
-    //     arr[i]=arr[i+1]
-    // }
-    // arr[arr.length-1] = temp
-    // console.log(arr);
-    
-    
-let arr = [10, 20, 30, 40, 50]
-let n =3
-let  temp =  arr[arr.length-1]
-
-// for(let i = arr.length-1 ; i>0 ; i--){
-//     arr[i]=arr[i-1]
-// }
-// arr[0] = temp
-// console.log(arr);
-
-for(let k =1 ; k<= n ; k++){
-    for(let i = arr.length-1 ; i>0 ; i--){
-        arr[i]=arr[i-1]
-    }
-    arr[0] = temp
-    temp = arr[arr.length-1]
-}
-console.log(arr);
