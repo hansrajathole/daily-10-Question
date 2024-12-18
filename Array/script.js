d29983bb9ca79f5ebeeaba7e1bc3942b417e4364
atholehansraj <hansrajathole1@gmail.com>
2024-12-18 22:16:24 +0530

    done

--- Array/script.js
+++ Array/script.js
@@ -158,3 +158,15 @@
 //     }
 // }
 // console.log(tp);
+
+
+let arr = [2,4,6,8,10]
+
+for(let i = 0 ; i<arr.length ; i++){
+    let str = ""
+    console.log(arr[i]);
+    for(let j = i + 1 ; j<arr.length ; j++){
+        str += arr[j]+" "   
+    }
+    console.log(str);  
+}
