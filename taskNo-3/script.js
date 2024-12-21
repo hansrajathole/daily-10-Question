be87d5a0ca5f2f44bc7d97224e2b1c974ad932c6
atholehansraj <hansrajathole1@gmail.com>
2024-12-21 23:37:51 +0530

    done

--- taskNo-3/script.js
+++ taskNo-3/script.js
@@ -1,42 +1,42 @@
 //========================================================
-function findMaxConsecutiveOnes(nums) {
-    let maxCount = 0;
-    let currentCount = 0;
+// function findMaxConsecutiveOnes(nums) {
+//     let maxCount = 0;
+//     let currentCount = 0;
 
-    for (let num of nums) {
-        if (num === 1) {
-            currentCount++;
-            maxCount = Math.max(maxCount, currentCount);
-        } else {
-            currentCount = 0;
-        }
-    }
+//     for (let num of nums) {
+//         if (num === 1) {
+//             currentCount++;
+//             maxCount = Math.max(maxCount, currentCount);
+//         } else {
+//             currentCount = 0;
+//         }
+//     }
 
-    return maxCount;
-}
+//     return maxCount;
+// }
 
-// Sample Input
-let nums = [1, 1, 0, 1, 1, 1];
-console.log(findMaxConsecutiveOnes(nums)); // Output: 3
+
+// let nums = [1, 1, 0, 1, 1, 1];
+// console.log(findMaxConsecutiveOnes(nums));
 
 
 
 //===================================================
-function findFrequency(nums) {
-    let frequencyMap = {};
+// function findFrequency(nums) {
+//     let frequencyMap = {};
 
-    for (let num of nums) {
-        frequencyMap[num] = (frequencyMap[num] || 0) + 1;
-    }
+//     for (let num of nums) {
+//         frequencyMap[num] = (frequencyMap[num] || 0) + 1;
+//     }
 
-    for (let key in frequencyMap) {
-        console.log(`${key}-${frequencyMap[key]}`);
-    }
-}
+//     for (let key in frequencyMap) {
+//         console.log(`${key}-${frequencyMap[key]}`);
+//     }
+// }
 
-// Sample Input
-let nums = [1, 5, 3, 1, 3, 5, 6];
-findFrequency(nums);
+
+// let nums = [1, 5, 3, 1, 3, 5, 6];
+// findFrequency(nums);
 
 
 
@@ -52,28 +52,23 @@ function thirdMax(nums) {
     }
 }
 
-// Sample Input 1
 let nums1 = [3, 2, 1];
-console.log(thirdMax(nums1)); // Output: 1
-
-// Sample Input 2
-let nums2 = [2, 1];
-console.log(thirdMax(nums2)); // Output: 2
+console.log(thirdMax(nums1));
 
 
 //=====================================================
-function minValueToBalance(nums) {
-    let n = nums.length / 2;
-    let leftSum = 0, rightSum = 0;
+// function minValueToBalance(nums) {
+//     let n = nums.length / 2;
+//     let leftSum = 0, rightSum = 0;
 
-    for (let i = 0; i < n; i++) {
-        leftSum += nums[i];
-        rightSum += nums[i + n];
-    }
+//     for (let i = 0; i < n; i++) {
+//         leftSum += nums[i];
+//         rightSum += nums[i + n];
+//     }
 
-    return Math.abs(leftSum - rightSum);
-}
+//     return Math.abs(leftSum - rightSum);
+// }
 
-// Sample Input
-let nums = [1, 2, 1, 2, 1, 3];
-console.log(minValueToBalance(nums)); // Output: 2
+// // Sample Input
+// let nums = [1, 2, 1, 2, 1, 3];
+// console.log(minValueToBalance(nums)); // Output: 2
