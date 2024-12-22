35da4a049a5d5312b4ab94abb846e9e0eab3d1b5
atholehansraj <hansrajathole1@gmail.com>
2024-12-22 22:39:44 +0530

    done

---
+++ practice/script.js
@@ -0,0 +1,40 @@
+//======================================
+//----find that array is shorted or not
+
+// let arr = [1,4,6,7,9,25,56,60]
+// let ans = false
+// for(let i = 0 ; i<arr.length-1 ; i++){
+//     if(arr[i+1]<arr[i]){
+//         ans = true
+//         break
+//     }
+// }
+// if(ans){
+//     console.log("this array is not shorted");
+// }else{
+//     console.log("this array is shorted"); 
+// }
+
+
+
+//=======================================
+//---- left rotation by one step --------
+
+// let arr = [10,20,30,40,50,60]
+// let temp = arr[arr.length-1]
+// for(let i = arr.length-1 ; i>0 ; i--){
+//     arr[i] = arr[i-1]
+// }
+// arr[0] = temp
+// console.log(arr);
+
+
+
+
+//======================================
+//----- right rotation by on step ------
+
+
+
+
+
