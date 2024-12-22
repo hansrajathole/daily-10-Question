9b028155eaf4acebe9bec60e58faad5028d10439
atholehansraj <hansrajathole1@gmail.com>
2024-12-23 00:17:15 +0530

    find the maximmum pair count of one

--- practice/script.js
+++ practice/script.js
@@ -129,3 +129,20 @@
 
 
 
+//============================================
+//----find the maximum pair count of one -----
+
+
+let arr = [1,1,0,0,1,1,0,1,1,1,0,1]
+let max = 0
+let count = 0
+for(let i = 0 ; i<arr.length ;i++){
+    if(arr[i]===1){
+        count++
+        max = Math.max(max,count)
+    }
+    else{
+        count = 0
+    }
+}
+console.log(`maximum pair of one ${max}`);
