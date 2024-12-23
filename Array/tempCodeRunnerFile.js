5f02ab2a9cd10e6924bbd83c8cbf2f426e1ce441
atholehansraj <hansrajathole1@gmail.com>
2024-12-23 09:53:56 +0530

    revesion and update

--- Array/tempCodeRunnerFile.js
+++ Array/tempCodeRunnerFile.js
@@ -1,2 +1,9 @@
-);
-// console.log(reverse(arr,2,arr.length-1));
+let arr =  [2,4,6,8,10]
+let tp = 0
+for(let i = 0 ; i<arr.length-1 ; i++){
+    for(let j = i + 1 ; j<arr.length ; j++){
+        console.log("("+arr[i]+","+arr[j]+")");
+        tp++
+    }
+}
+console.log(tp);
---
+++ practice/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+let arr = [10,20,30,40,50,60]
+let temp = arr[0]
+for(let i = 0 ; i<arr.length-1 ; i++){
+    arr[i] = arr[i+1]
+}
+arr[arr.length-1] = temp
+console.log(arr);
