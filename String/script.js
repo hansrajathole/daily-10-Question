4cd0fca37d94a63e7159387d64dc325898f89dc7
atholehansraj <hansrajathole1@gmail.com>
2024-12-25 14:21:27 +0530

    reverse string done

--- String/script.js
+++ String/script.js
@@ -12,3 +12,13 @@
 //     console.log(s[i]);
 // }
 
+//==========================================
+// reverse string 
+// let str = "hello"
+// let str2 = ''
+// for(let i = str.length-1 ; i>=0 ;i--){
+//     str2 += str.charAt(i)
+// }
+// console.log(str2);
+
+
