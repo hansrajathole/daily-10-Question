1987e2f214a6247ff2d76d37f15685dfaf2b4b1a
atholehansraj <hansrajathole1@gmail.com>
2024-12-25 13:53:20 +0530

    question 1 done

---
+++ String/script.js
@@ -0,0 +1,14 @@
+//===================================================
+// print all charactor in next line
+// let s = "hansraj"
+
+//first way
+// for(let i = 0 ; i<s.length ; i++){
+//     console.log(s.charAt(i));
+// }
+
+// second way
+// for(let i = 0 ; i<s.length ; i++){
+//     console.log(s[i]);
+// }
+
