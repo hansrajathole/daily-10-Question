05de556e16cf90b6622642f6496b272ee5dfa14a
atholehansraj <hansrajathole1@gmail.com>
2024-12-25 14:36:20 +0530

    check palindrom or not done

--- String/script.js
+++ String/script.js
@@ -22,3 +22,34 @@
 // console.log(str2);
 
 
+//==========================================
+// check the string pallindrom or not 
+
+let str = "mom"
+let str2 = ''
+
+//first way
+// for(let i = str.length-1 ; i>=0 ;i--){
+//     str2 += str.charAt(i)
+// }
+
+// if(str === str2)console.log("pallindrom string");
+// else console.log("not pallindrom string");
+
+
+// second way 
+let i = 0 
+let j = str.length-1
+let ispall = true
+while(i<j){
+    if(str.charAt(i)!==str.charAt(j)){
+        ispall = false
+        break
+    }   
+    i++
+    j--    
+}
+
+if(ispall)console.log("pallindrom");
+else console.log("not pallindrome");
+
