fd71975e675ad7386960926669f82285796b09c4
atholehansraj <hansrajathole1@gmail.com>
2025-01-20 12:12:52 +0530

    recursion start

---
+++ recursion/script.js
@@ -0,0 +1,42 @@
+const prompt = require("prompt-sync")({sigint: true});
+let n = Number(prompt("Enter a number: "));
+
+//================================================================
+// ======================== print hello world ====================
+
+// let printHello = (n)=>{
+//     if(n<=0){
+//         return;
+//     }
+//     console.log("hello world");
+//     printHello(n-1);
+    
+// }
+// printHello(n)
+
+
+
+//================================================================
+// ======================== print 1 to n =========================
+
+let print1ToN = (i,n)=>{
+    if(i>n){
+        return;
+    }
+    console.log(i);    
+    print1ToN(i+1,n);
+}
+print1ToN(1,n)
+
+
+//================================================================
+//========================= print n to 1 =========================
+
+// let printNTo1 = (n)=>{
+//     if(n<=0){
+//         return;
+//     }
+//     console.log(n);    
+//     printNTo1(n-1);
+// }
+// printNTo1(n)
