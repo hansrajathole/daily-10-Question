4cef5760c32700df61d7a8f5395c3a9248ce52f1
atholehansraj <hansrajathole1@gmail.com>
2025-01-23 12:34:36 +0530

    factor in loop with sorted

--- recursion/script.js
+++ recursion/script.js
@@ -1,5 +1,5 @@
 const prompt = require("prompt-sync")({sigint: true});
-let n = Number(prompt("Enter a number: "));
+// let n = Number(prompt("Enter a number: "));
 
 //================================================================
 // ======================== print hello world ====================
@@ -99,9 +99,96 @@ let n = Number(prompt("Enter a number: "));
 
 //==============================================================
 //========================= fibo series ========================
-let fiboSeries = (i,j,n)=>{  
-    console.log(j);
-    if(n==1) return;
-    fiboSeries(j,i+j,n-1);
+// let fiboSeries = (i,j,n)=>{  
+//     console.log(j);
+//     if(n==1) return;
+//     fiboSeries(j,i+j,n-1);
+// }
+// fiboSeries(0,1,n);
+
+
+//==============================================================
+//========================== GCD  ==============================
+// let a = Number(prompt("Enter a number: "));
+// let b = Number(prompt("Enter b number: "));
+// let i = Math.min(a,b);
+
+// let gcd = (a,b,i)=>{
+//     if(i==1){
+//         return 1;
+//     }
+//     if(a%i==0 && b%i==0){
+//         return i;
+//     }
+//     return gcd(a,b,i-1);
+// }
+// console.log(gcd(a,b,i));
+
+
+// second method
+// let gcd = (a,b)=>{
+//     if(a===b)return a;
+//     if(a>b)return gcd(a-b,b);
+//        return gcd(a,b-a);
+   
+// }
+// console.log(gcd(a,b));
+
+//==============================================================
+//========================== factor ============================
+let n = Number(prompt("Enter a number: "));
+// let factor = (n,i)=>{
+//     if(i>n/2){
+//         console.log(n);
+//         return;
+//     }
+//     if(n%i==0){
+//         console.log(i);
+//     }
+//     factor(n,i+1);
+// }   
+// factor(n,1);
+
+
+// let factor = (n,i)=>{
+//         if(i>n/2){
+//             console.log(n);
+//             return;
+//         }
+//         if(n%i==0){
+//             console.log(i);
+//         }
+//         factor(n,i+1);
+//     }   
+//     factor(n,1);
+
+// }   
+
+// factor in loop 
+// for(let i = 1 ; i<=Math.floor(Math.sqrt(n));i++){
+//     if(n%i===0){
+//         if(n/i==i){
+//             console.log(i);
+//         }else{
+//             console.log(i);
+//             console.log(n/i);
+//         }
+//     }
+// }
+
+// factor in loop with sorted
+let i = 0
+for(i = 1; i*i < n ; i++){
+    if(n%i===0){
+        console.log(i);
+    }
 }
-fiboSeries(0,1,n);
+if((i-n/i)==1)i--
+for(;i>=1 ; i--){
+    if(n%i == 0){
+        console.log(n/i);
+    }
+}
+
+
+//==============================================================
