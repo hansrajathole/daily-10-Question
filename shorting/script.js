6770dc59ab8a5d00bf71d4b50e7f638ecd48b3bc
atholehansraj <hansrajathole1@gmail.com>
2025-02-12 23:40:36 +0530

    counting sort

--- shorting/script.js
+++ shorting/script.js
@@ -3,52 +3,53 @@ const prompt = require("prompt-sync")({sigint: true});
 // ========================== insertion sort ===========================
 
 //     let arr = [12, 11, 13, 5, 6];
-//     for (let i = 1; i < arr.length; i++) {
+//     for(let i = 1 ; i<arr.length ; i++){
 //         let key = arr[i];
-//         let j = i - 1;
-//         while (j >= 0 && arr[j] > key) {
-//             arr[j + 1] = arr[j];
-//             j = j - 1;
+//         let j = i-1
+//         while(j>= 0 && arr[j]>key){
+//             arr[j+1] = arr[j];
+//             j--
 //         }
-//         arr[j + 1] = key;
+//         arr[j+1] = key
 //     }
 
 // console.log(arr);
 
 
 //====================================================================
-//========================== selection sortt =========================
+//========================== selection sort  =========================
 
 // let arr = [7, 4, 3, 5, 6, 9,8]
+
 // for(let i = 0 ; i<arr.length-1 ; i++){
-//     minIndex = i
-//     for(let j = i + 1 ; j<arr.length ; j++){
-//         if(arr[j]<arr[minIndex]){
+//     let minIndex = i
+//     for(let j = i+ 1 ; j<arr.length ; j++){
+//         if(arr[j]< arr[minIndex]){
 //             minIndex = j
 //         }
 //     }
 //     let temp = arr[minIndex]
 //     arr[minIndex] = arr[i]
 //     arr[i] = temp
-
 // }
-// console.log(arr);
- 
 
+// console.log(arr);
 
 //==================================================================
 //========================= bubble sort ============================
 
-// let arr = [7, 4, 3, 5, 6, 9,8]
-// for(let i = 0 ; i<arr.length-1 ;i++){
-//     for(let j = 0 ; j<arr.length-i-1 ;  j++){
+// let arr = [5,4,1,3,2]
+
+// for(let i = 0 ; i<arr.length-1 ; i++){
+//     for(let j = 0 ; j<arr.length-i-1 ; j++){
 //         if(arr[j]>arr[j+1]){
 //             let temp = arr[j]
-//             arr[j] = arr[j+1] 
-//             arr[j+1] = temp  
+//             arr[j] = arr[j+1]
+//             arr[j+1] =temp
 //         }
 //     }
 // }
+
 
 // console.log(arr);
 
@@ -76,33 +77,19 @@ const prompt = require("prompt-sync")({sigint: true});
 
 //     while(i<=mid && j<=last){
     
-//         if(arr[i]<arr[j]){
-//             newArr[k++] = arr[i++]
-//         }else{
-//             newArr[k++] = arr[j++]
-//         }
+//         if(arr[i]<arr[j]) newArr[k++] = arr[i++]
+//         else newArr[k++] = arr[j++]
+//         
 //     }
-
 //     while(i<=mid){
 //         newArr[k++] = arr[i++]
 //     }
-    
 //     while(j<=last){
 //         newArr[k++] = arr[j++]
 //     }
-
-//     for(let i = 0 ; i<newArr.length ; i++){
-//         console.log(newArr[i])
-//     }
-//     console.log(" ");
-    
-
-    
 //     for(let i = 0 ,j = first ; i<newArr.length ; i++, j++){
 //         arr[j] = newArr[i]
 //     }
-//     console.log(first ," ", last);
-    
 // }
 
 // console.log(arr);
@@ -150,25 +137,54 @@ const prompt = require("prompt-sync")({sigint: true});
 //===========================================================
 //=========================== Cyclic sort ====================
 
-let arr = [1, 5, 6, 4, 3, 2, 8, 7]
+// let arr = [1, 5, 6, 4, 3, 2, 8, 7]
 
-cyclicSort(arr)
+// cyclicSort(arr)
 
-function cyclicSort(arr) {
-    let n = arr.length
-    let i = 0
-    while( i < n) {
-        let correctIndex = arr[i] - 1
-        if (arr[i] != arr[correctIndex]) {
-            let temp = arr[i]
-            arr[i] = arr[correctIndex]
-            arr[correctIndex] = temp
-            correctIndex = arr[i] - 1
-        }else{
-            i++
-        }
-    }
-    console.log(arr)
-}
+// function cyclicSort(arr) {
+//     let n = arr.length
+//     let i = 0
+//     while( i < n) {
+//         let correctIndex = arr[i] - 1
+//         if (arr[i] != arr[correctIndex]) {
+//             let temp = arr[i]
+//             arr[i] = arr[correctIndex]
+//             arr[correctIndex] = temp
+//             correctIndex = arr[i] - 1
+//         }else{
+//             i++
+//         }
+//     }
+//     console.log(arr)
+// }
 
 // console.log(arr);
+
+
+//=============================================================
+//=================== Counting Sort ===========================
+
+
+let arr = [1,4,1,3,2,4,3,7]
+
+let largest = -Infinity
+for(let i = 0 ; i<arr.length ; i++){
+    if(arr[i]>largest) largest = arr[i]
+}
+
+let newArr = new Array(largest+1).fill(0)
+for(let i = 0 ; i<arr.length ; i++){
+    newArr[arr[i]]++
+}
+console.log(newArr);
+
+let j = 0
+for(let i = 0 ; i<arr.length ; i++){
+    while(newArr[i]>0){
+        arr[j] = i
+        j++
+        newArr[i]--
+    }
+}
+
+console.log(arr);
