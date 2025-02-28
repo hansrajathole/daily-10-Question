18da986a43410b58902922ebe81f715341536ac4
hansrajathole <hansrajathole1@gmail.com>
2025-02-28 09:54:51 +0530

    implement linked list with insertion, deletion, and print methods

--- Linked-List/script.js
+++ Linked-List/script.js
@@ -1,22 +1,144 @@
-class Person {
-    constructor(name, age) {
-      this.name = name;
-      this.age = age;
-    }
-  
-    // Method
-    greet() {
-      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-    }
+class Node {
+  constructor(val) {
+      this.val = val;
+      this.next = null;
+  }
+}
+class LinkedList {
+  constructor() {
+      this.head = null
+      this.size = 0
+  }
+  insertAtFirst(val) {
+      this.size++
+      let newNode = new Node(val);
+      if (this.head == null) {
+          this.head = newNode
+          return
+      }
+      newNode.next = this.head
+      this.head = newNode
   }
 
-  
-const person1 = new Person("Hansraj", 24);
-const person2 = new Person("Athole", 22);
+  insertAtLast(val){
+   this.size++
+   let newNode = new Node(val);
+   if(this.head == null){
+      this.head = newNode;
+      return
+   }
+   let cur = this.head;
+   while(cur.next != null){
+     cur = cur.next;
+   }
+   cur.next = newNode
+  }
 
-person1.greet(); 
-person2.greet(); 
+  deleteAtFirst(){
+      this.size--
+      if(this.head == null){
+          console.log("no node found");
+          return 
+      }   
+      if(this.head.next == null){
+          this.head = null;
+          return
+      }
+      this.head = this.head.next
+  }
+
+  deleteAtLast(){
+      this.size--
+      if(this.head == null || this.head.next == null){
+          this.head = null
+          return
+      }
+      let cur = this.head;
+      while(cur.next.next != null){
+        cur = cur.next;
+      }
+      cur.next = null
+  }
+  insertAtPos(val , pos){
+     if(pos<1 || this.size+1<pos){
+      console.log("out of bound");
+      return
+     }
+     if(pos==1){
+      this.insertAtFirst(val)
+      return
+     }
+     if(pos==this.size+1){
+      this.insertAtLast(val)
+      return
+     }
+     this.size++;
+     let cur = this.head
+     let i =1;
+     while(i<pos-1){
+      cur = cur.next;
+      i++
+     }
+     let newNode = new Node(val)
+     newNode.next = cur.next
+     cur.next = newNode
+  }
+  deleteAtPos(pos){
+    if(pos<1 || this.size<pos){
+      console.log("out of bound");
+      return
+    }
+    if(pos==1){
+      this.deleteAtFirst()
+      return
+    }
+    if(pos==this.size){
+      this.deleteAtLast()
+      return
+    }
+    this.size--;
+    let i = 1;
+    let cur = this.head;
+    while(i<pos-1){
+      cur = cur.next;
+      i++
+    }
+    cur.next = cur.next.next
+  }
+
+  print(){
+      if(this.head == null){
+          console.log("no found node");
+          return 
+      }
+      let cur = this.head;
+      let arr = []
+      while(cur!=null){
+          arr.push(cur.val)
+          cur = cur.next
+      }
+      console.log(arr.join("->")+"->null");
+  }
+}
 
 
-// console.log(person1);
-// console.log(person2);
+let obj = new LinkedList(50)
+
+
+
+obj.insertAtFirst(10);
+obj.insertAtFirst(20);
+obj.insertAtFirst(30);
+obj.print()
+obj.insertAtLast(40);
+obj.insertAtLast(50);
+obj.insertAtLast(60);
+obj.print()
+obj.deleteAtFirst();
+obj.print()
+obj.deleteAtLast();
+obj.print()
+// obj.insertAtPos(150,10)
+// obj.print()
+// obj.deleteAtPos(3);
+// obj.print()
