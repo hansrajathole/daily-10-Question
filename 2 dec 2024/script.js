196250d58f4e01f1458140f4a62b3cda1c4e366f
hansrajathole <hansrajathole1@gmail.com>
2025-04-04 19:02:49 +0530

    refactor getsum function to use apply method and update parameter handling

--- 2 dec 2024/script.js
+++ 2 dec 2024/script.js
@@ -1,19 +1,16 @@
 
 
-let arr =[-5,-4,-3,-2,0,2,4,6,8]
 
-function getsum(arr){
-
-
-    for(let i = 0 ; i <arr.length ; i++){
-        
-        for(let j =0 ; j<arr.length ; j++){
-            
-            if(arr[i]+arr[j]==0){
-                return [arr[i],arr[j]]
-            }
-        }
-    }
-
+let obj ={
+    name : "sahil",
+    age : 22,
+   
 }
-console.log(getsum(arr));
+function getsum(a,b){
+
+    console.log(this);
+    console.log(a,b);
+    
+    
+}
+getsum.apply(obj,[1,2])
